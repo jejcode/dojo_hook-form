c65ae88dc745f024eab78425656e4e3c4589d5a8
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-05-04 20:53:34 -0600

    added validation and submit button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,74 @@
 import React, {useState} from 'react'
 
 const Form = (props) => { // make a useState for each piece of form data
-    const [firstName, SetFirstName] = useState("")
-    const [lastName, SetLastName] = useState("")
-    const [email, SetEmail] = useState("")
-    const [password, SetPassword] = useState("")
-    const [confirmPassword, SetConfirmPassword] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [firstNameError, setFirstNameError] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [lastNameError, setLastNameError] = useState("")
+    const [email, setEmail] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [confirmPasswordError, setConfirmPasswordError] = useState("")
 
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value)
+        if(e.target.value.length > 0 && e.target.value.length < 2) {
+            setFirstNameError("First name must be at least 2 characters!")
+        } else {
+            setFirstNameError("")
+        }
+    }
+    const handleLastName = (e) => {
+        setLastName(e.target.value)
+        if(e.target.value.length > 0 && e.target.value.length < 2) {
+            setLastNameError("Last name must be at least 2 characters!")
+        } else {
+            setLastNameError("")
+        }
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+        if(e.target.value.length > 0 && e.target.value.length < 5) {
+            setEmailError("Email must be at least 3 characters!")
+        } else {
+            setEmailError("")
+        }
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+        if(e.target.value.length > 0 && e.target.value.length < 8) {
+            setPasswordError("Password must be at least 8 characters!")
+        } else {
+            setPasswordError("")
+        }
+        if(e.target.value !== confirmPassword && confirmPassword.length > 0) {
+            setConfirmPasswordError("Confirm password does not match!")
+        } else {
+            setConfirmPasswordError("")
+        }
+    }
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value)
+        if(e.target.value !== password && e.target.value.length > 0) {
+            setConfirmPasswordError("Confirm password does not match!")
+        } else {
+            setConfirmPasswordError("")
+        }
+    }
     const createUser = (e) => {
         e.preventDefault() // prevent default behavior of resetting the page
 
         const newUser = {firstName, lastName, email, password} // make an object out of newUser data
-        console.log(`Welcome, ${newUser}`)
-        SetFirstName("")
-        SetLastName("")
-        SetEmail("")
-        SetPassword("")
-        SetConfirmPassword("")
+        console.log(`Welcome, ${newUser.firstName}`)
+        setFirstName("")
+        setLastName("")
+        setEmail("")
+        setPassword("")
+        setConfirmPassword("")
     }
+
     return (
         <div className='row justify-content-center'>
             <div className='col-4'>
@@ -25,26 +76,56 @@ const Form = (props) => { // make a useState for each piece of form data
                 <form className='form m-4' onSubmit={ createUser }>
                     <div className='mb-4'>
                         <label className='form-label'>First Name:</label>
-                        <input className='form-control' type="text" onChange={ (e) => SetFirstName(e.target.value)} value={ firstName }/>
+                        <input className='form-control' type="text" onChange={ (e) => handleFirstName(e)} value={ firstName }/>
+                        {
+                            firstNameError ?
+                            <p className='text-danger'>{ firstNameError }</p> :
+                            ''
+                        }
                     </div>
                     <div className='mb-4'>
                         <label className='form-label'>Last Name:</label>
-                        <input className='form-control' type="text" onChange={ (e) => SetLastName(e.target.value) } value={ lastName }/>
+                        <input className='form-control' type="text" onChange={ (e) => handleLastName(e) } value={ lastName }/>
+                        {
+                            lastNameError ?
+                            <p className='text-danger'>{ lastNameError }</p> :
+                            ''
+                        }
                     </div>
                     <div className='mb-4'>
                         <label className='form-label'>Email:</label>
-                        <input className='form-control' type="text" onChange={ (e) => SetEmail(e.target.value) } value={ email }/>
+                        <input className='form-control' type="text" onChange={ (e) => handleEmail(e) } value={ email }/>
+                        {
+                            emailError ?
+                            <p className="text-danger">{ emailError }</p> :
+                            ''
+                        }
                     </div>
                     <div className='mb-4'>
                         <label className='form-label'>Password:</label>
-                        <input className='form-control' type="password" onChange={ (e) => SetPassword(e.target.value) } value={ password }/>
+                        <input className='form-control' type="password" onChange={ (e) => handlePassword(e) } value={ password }/>
+                        {
+                            passwordError ?
+                            <p className="text-danger">{ passwordError }</p> :
+                            ''
+                        }
                     </div>
                     <div className='mb-4'>
                         <label className='form-label'>Confirm Password:</label>
-                        <input className='form-control' type="password" onChange={ (e) => SetConfirmPassword(e.target.value) } value={ confirmPassword }/>
+                        <input className='form-control' type="password" onChange={ (e) => handleConfirmPassword(e) } value={ confirmPassword }/>
+                        {
+                            confirmPasswordError ?
+                            <p className="text-danger">{ confirmPasswordError }</p> :
+                            ''
+                        }
                     </div>
+                    {
+                        firstNameError || lastNameError || emailError || passwordError || confirmPasswordError ?
+                        <input type="submit" value="Create User" disabled/> :
+                        <input type="submit" value="Create User" />
+                    }
                 </form>
-
+                <hr></hr>
                 <table className='table table-light caption-top'>
                     <caption>Your Form Data:</caption>
                     <tbody>
